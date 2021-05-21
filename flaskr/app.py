24b069dffe5ae69fe811a5d4a3c026f10f75046b
Eli Talcott <eli.talcott@gmail.com>
2021-05-21 13:13:18 -0400

    Started using Bootstrap for responsive web development and added package requirements and SBOM

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,6 @@
 """Flask application to present flipping information"""
 
 import locale
-from time import time
 from flask import Flask, render_template
 from flaskr.runelite_api import get_latest
 from flaskr.item_utilities import get_buy_limit_from_name, get_item_sets_name, get_id_from_name, \
@@ -12,13 +11,13 @@ app = Flask(__name__)
 locale.setlocale(locale.LC_ALL, "en_US.UTF8")
 
 @app.route("/")
-def hello():
+def index():
     """
     Homepage. Will eventually include buttons to
     navigate to various flipping utility pages
     :return: Welcome message
     """
-    return "Welcome to my OSRS flipping utility!"
+    return render_template("index.html")
 
 @app.route('/sets_to_components')
 def item_sets_to_components():
@@ -31,11 +30,10 @@ def item_sets_to_components():
     latest = get_latest()
 
     # Fill in set profit data
-    headings = ["Set", "Components", "Set Price", "Components Price", "Margin",
-                "ROI (%)", "Buy Limit", "Daily Volume", "Margin * Buy Limit"]
+    headings = ["Set", "Price", "Components", "Price", "Margin",
+                "ROI", "Buy Limit", "Daily Volume", "Margin * Buy Limit"]
     sets = get_item_sets_name()
     data = []
-    start = time()
     try:
         for key, val in sets.items():
             set_cost = latest["data"][str(get_id_from_name(key))]["high"]
@@ -46,7 +44,7 @@ def item_sets_to_components():
                 components_cost += latest["data"][str(get_id_from_name(comp))]["low"]
             margin, roi = calc_profit_roi(set_cost, components_cost)
             buy_limit = min([get_buy_limit_from_name(val) for val in val["components"]])
-            data.append([key, components[:-2], format(set_cost, ",d"),
+            data.append([key, format(set_cost, ",d"), components[:-2],
                          format(components_cost, ",d"), format(int(margin), ",d"), roi.__round__(1),
                          format(buy_limit, ",d"), get_volume_from_name(key),
                          format(int(margin * buy_limit), ",d")])
@@ -56,7 +54,6 @@ def item_sets_to_components():
     # data.sort(key=lambda x: locale.atoi(x[5]), reverse=True)
     # Sort by ROI
     data.sort(key=lambda x: x[5], reverse=True)
-    print("Time to load all stuff: {}".format(time() - start))
 
     # Calculate profit
     return render_template("item_sets.html", headings=headings, data=data)
